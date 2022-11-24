189e189b7354227fb3d35c8947f995945aebb9c3
Gregory Greenfield <Gregorysmgreenfield@gmail.com>
2022-11-24 13:13:23 +0000

    Final version used in the workshop.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace DebugLogsRunningForm
+{
+    partial class DebugLogsRunning
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DebugLogsRunning));
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 16);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(131, 150);
+            this.txtBox.TabIndex = 0;
+            this.txtBox.Text = "";
+            this.txtBox.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(62, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Hello world!";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(149, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 75);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 75);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(373, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 75);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(485, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 75);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(597, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 75);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(149, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 75);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(261, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 75);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(373, 91);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 75);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "10";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.White;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(485, 91);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(106, 75);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "11";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.White;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(597, 91);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(106, 75);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "12";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DebugLogsRunning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 175);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DebugLogsRunning";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Is Debug Running?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DebugLogsRunningForm
+{
+    public partial class DebugLogsRunning : Form
+    {
+        public DebugLogsRunning()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {            
+            // Set the Multiline property to true.
+            txtBox.Multiline = true;
+            // Add vertical scroll bars to the TextBox control.
+            txtBox.AcceptsTab = true;
+            // Set WordWrap to true to allow text to wrap to the next line.
+            txtBox.WordWrap = true;
+            // Set the default text of the control.
+            txtBox.Text = "Welcome!" + Environment.NewLine + "Second Line";
+
+            lblMessage.Text = DateTime.Now.ToString();
+
+            string path_generator(string suite_number)
+            {
+                return "W:\\DebugLogs\\Suite " + suite_number + "\\Linac" + System.DateTime.Now.ToString("ddd") + "Log.txt";
+            }
+
+            DateTime LastWriteTime(string suite_number)
+            {
+                string this_path = path_generator(suite_number);
+                return File.GetLastWriteTime(this_path);
+            }
+
+            void background_colour_green(string suite_number)
+            {
+                if (suite_number == "1")
+                {
+                    label1.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "2")
+                {
+                    label2.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "3")
+                {
+                    label3.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "4")
+                {
+                    label4.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "5")
+                {
+                    label5.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "6")
+                {
+                    label6.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "8")
+                {
+                    label8.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "10")
+                {
+                    label10.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "11")
+                {
+                    label11.BackColor = Color.MediumSeaGreen;
+                }
+                if (suite_number == "12")
+                {
+                    label12.BackColor = Color.MediumSeaGreen;
+                }
+            }
+
+            void background_colour_red(string suite_number)
+            {
+                if (suite_number == "1")
+                {
+                    label1.BackColor = Color.Red;
+                }
+                if (suite_number == "2")
+                {
+                    label2.BackColor = Color.Red;
+                }
+                if (suite_number == "3")
+                {
+                    label3.BackColor = Color.Red;
+                }
+                if (suite_number == "4")
+                {
+                    label4.BackColor = Color.Red;
+                }
+                if (suite_number == "5")
+                {
+                    label5.BackColor = Color.Red;
+                }
+                if (suite_number == "6")
+                {
+                    label6.BackColor = Color.Red;
+                }
+                if (suite_number == "8")
+                {
+                    label8.BackColor = Color.Red;
+                }
+                if (suite_number == "10")
+                {
+                    label10.BackColor = Color.Red;
+                }
+                if (suite_number == "11")
+                {
+                    label11.BackColor = Color.Red;
+                }
+                if (suite_number == "12")
+                {
+                    label12.BackColor = Color.Red;
+                }
+            }
+            
+            string delta_change(string suite_number)
+            {
+                DateTime now_minus_six = DateTime.Now.Subtract(new TimeSpan(0, 25, 0));
+                DateTime last_update = LastWriteTime(suite_number);
+                int comparison = last_update.CompareTo(now_minus_six);
+                if (comparison >= 1)
+                {
+                    background_colour_green(suite_number);
+                    return last_update.ToString("T");
+                    //return "Y";
+                }
+                else
+                {
+                    background_colour_red(suite_number);
+                    return last_update.ToString("T");
+                    //return "    N";
+                }
+            }
+
+            void Update_all()
+            {
+                string Update_1 = delta_change("1");
+                string Update_2 = delta_change("2");
+                string Update_3 = delta_change("3");
+                string Update_4 = delta_change("4");
+                string Update_5 = delta_change("5");
+                string Update_6 = delta_change("6");
+                string Update_8 = delta_change("8");
+                string Update_10 = delta_change("10");
+                string Update_11 = delta_change("11");
+                string Update_12 = delta_change("12");
+
+                txtBox.Text = "Suite\tLast Updated" + Environment.NewLine + "S1\t" + Update_1 + Environment.NewLine + "S2\t" + Update_2 + Environment.NewLine + "S3\t" + Update_3 + Environment.NewLine + "S4\t" + Update_4 + Environment.NewLine + "S5\t" + Update_5 + Environment.NewLine + "S6\t" + Update_6 + Environment.NewLine + "S8\t" + Update_8 + Environment.NewLine + "S10\t" + Update_10 + Environment.NewLine + "S11\t" + Update_11 + Environment.NewLine + "S12\t" + Update_12;
+            }
+            Update_all();
+        }
+    }
+}
